a1beb937b9fe7fb7fde345d26cd6e83cc7eae1d0
Ridote <daniridao@gmail.com>
2018-12-09 22:28:43 +0000

    Piggy movement oink oink

--- Entities/Characters/Character.gd
+++ Entities/Characters/Character.gd
@@ -1,52 +1,56 @@
 extends Node2D
 
-const FLOOR_NORMAL = Vector2(0, 1)
+const FLOOR_NORMAL = Vector2(0, -1)
 const SLOPE_SLIDE_STOP = 25.0
 const WALK_SPEED = 150 # pixels/sec
+const GRAVITY = 500
+const JUMP_SPEED = 15000
 const SIDING_CHANGE_SPEED = 10
 const STOP_ANIMATION_THRESHOLD = 5
 
-const EXTERNAL_IMPULSE = 4000
-const IMPULSE_MITIGATION_FACTOR = 2
-
-const MAX_SPEED_AND_IMPULSE = 400
 
 var linear_vel : Vector2 = Vector2(0,0)
 var target_vel : Vector2 = Vector2(0,0)
 var prev_anim : String = ""
 
-var externalImpulse = Vector2()
 
 func _ready():
 	add_to_group(Constants.G_CHARACTER)
 	
 	
 func move(_delta : float, direction : Vector2) -> void:
-	target_vel *= WALK_SPEED*direction.normalized()
-
-	linear_vel.x = lerp(linear_vel.x, target_vel.x + externalImpulse.x, 0.1)
-	linear_vel.y = lerp(linear_vel.y, target_vel.y + externalImpulse.y, 0.1)
+	target_vel.x = WALK_SPEED*direction.x
+	target_vel.y = JUMP_SPEED*direction.y
 	
-	#We clamp the linear velocity
-	linear_vel = linear_vel.clamped(MAX_SPEED_AND_IMPULSE)
+	linear_vel.x = lerp(linear_vel.x, target_vel.x, 0.2)
+	linear_vel.y = lerp(linear_vel.y, target_vel.y + GRAVITY, 0.02)
+
 	linear_vel = $body.move_and_slide(linear_vel, FLOOR_NORMAL, SLOPE_SLIDE_STOP)
-	externalImpulse /= IMPULSE_MITIGATION_FACTOR
 
 func animate(animation : String):
 	if animation != prev_anim:
 		$AnimationPlayer.play(animation)
 		prev_anim = animation
 
-func receiveDmg(dmg : float, _source : Object):
-	var sourcePos = _source.getGlobalPosition()
-	var direction = ($body.global_position - sourcePos).normalized()
-	externalImpulse += direction*EXTERNAL_IMPULSE
+func receiveDmg(_dmg : float, _source : Object):
+	#var sourcePos = _source.getGlobalPosition()
+	#var direction = ($body.global_position - sourcePos).normalized()
+	#externalImpulse += direction*EXTERNAL_IMPULSE
 	# TODO DMG
-
+	pass
 
 ################################################ Position
-func getGlobalPosition():
+func getGlobalPosition() -> Vector2:
 	return $body.global_position
 
-func setGlobalPosition(newPos):
+func setGlobalPosition(newPos : Vector2) -> void:
 	$body.global_position = newPos
+
+func is_on_floor() -> bool:
+	return $body.is_on_floor()
+
+func is_on_wall() -> bool:
+	return $body.is_on_wall()
+
+func is_on_ceilling() -> bool:
+	return $body.is_on_ceilling()
--- Entities/Characters/Player/Player.gd
+++ Entities/Characters/Player/Player.gd
@@ -8,5 +8,17 @@ extends "res://Entities/Characters/Character.gd"
 func _ready():
 	add_to_group(Constants.G_PLAYER)
 
-func input():
-	pass
+func _physics_process(delta):
+	var direction = input()
+	move(delta, direction)
+
+func input() -> Vector2:
+	var direction = Vector2(0,0)
+	
+	if(Input.is_action_pressed("ui_up") && is_on_floor()):
+		direction.y = -1
+	if(Input.is_action_pressed("ui_left")):
+		direction.x += -1
+	elif(Input.is_action_pressed("ui_right")):
+		direction.x += 1
+	return direction
